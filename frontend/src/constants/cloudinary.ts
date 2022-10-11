1b41a404ef092a20e28bfba8dd42eb19edc1906e
Daniel Leal <cosaleal@gmail.com>
2022-10-11 02:00:50 -0300

    Add a button to open the uploaded image

--- frontend/src/constants/cloudinary.ts
+++ frontend/src/constants/cloudinary.ts
@@ -1,3 +1,3 @@
-export const CLOUD_NAME = import.meta.env.VITE_VITE_CLOUDINARY_CLOUD_NAME;
-export const UPLOAD_PRESET = import.meta.env.VITE_VITE_CLOUDINARY_UPLOAD_PRESET;
-export const FOLDER = import.meta.env.VITE_VITE_CLOUDINARY_FOLDER;
+export const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
+export const FOLDER = import.meta.env.VITE_CLOUDINARY_FOLDER;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import * as cloudinary from '@/services/cloudinary';
 import FileHandler from '@/components/FileHandler.vue';
 
 import type { Nullable } from '@/types/utils';
 
+const uploadedUrl = ref('');
+
 const fileSelected = async (file: Nullable<File>) => {
   if (file) {
     const { url } = await cloudinary.upload(file);
-    console.log(url);
+    uploadedUrl.value = url;
   }
 };
 </script>
 
 <template>
   <FileHandler @file-selected="fileSelected" />
+
+  <div
+    v-if="uploadedUrl"
+    class="w-full text-center mt-4"
+  >
+    <a
+      :href="uploadedUrl"
+      class="font-medium text-fuchsia-500 hover:underline"
+      target="_blank"
+    >
+      Ver imagen!
+    </a>
+  </div>
 </template>
