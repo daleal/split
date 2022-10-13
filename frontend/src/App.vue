1b6cbe2072cdf983c4d63634366b61aaff83267d
Daniel Leal <cosaleal@gmail.com>
2022-10-13 00:11:40 -0300

    Drop the use of the viewport

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import PageFooter from '@/components/layout/PageFooter.vue';
 </script>
 
 <template>
-  <div class="w-screen h-screen overflow-x-hidden flex flex-col justify-between">
+  <div class="w-full h-full overflow-x-hidden flex flex-col justify-between">
     <div class="container max-w-md mx-auto mb-6 px-2 md:px-0">
       <router-view />
     </div>
