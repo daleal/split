c69f149d01da97f17bd47c8402d382c60a0dda5d
Daniel Leal <cosaleal@gmail.com>
2022-10-16 15:07:56 -0300

    Add a distinctive class to the main app container

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ useViewHeightHack();
 </script>
 
 <template>
-  <div class="w-full h-screen overflow-x-hidden flex flex-col justify-between">
+  <div class="main-container w-full h-screen overflow-x-hidden flex flex-col justify-between">
     <div class="container max-w-md mx-auto mb-6 px-2 md:px-0">
       <router-view />
     </div>
