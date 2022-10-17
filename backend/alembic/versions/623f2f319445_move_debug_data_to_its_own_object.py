44ca2bd4ec0fb9e95beaea9fb35b997e1f494322
Daniel Leal <cosaleal@gmail.com>
2022-10-17 20:37:27 -0300

    Create new model associations

---
+++ backend/alembic/versions/623f2f319445_move_debug_data_to_its_own_object.py
@@ -0,0 +1,24 @@
+"""Move debug data to its own object
+
+Revision ID: 623f2f319445
+Revises: 9b27e2ae0cbd
+Create Date: 2022-10-17 23:21:00.074882
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '623f2f319445'
+down_revision = '9b27e2ae0cbd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- backend/split/models/bill.py
+++ backend/split/models/bill.py
@@ -1,14 +1,16 @@
+from __future__ import annotations
+
 import uuid
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, Column, String
+from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import ARRAY, UUID
 from sqlalchemy.orm import relationship
 
 from split.shared.models import BaseModel
 
 if TYPE_CHECKING:
-    from split.models import Item, Participant
+    from split.models import Item, ItemGeneration, Participant
 
 
 class Bill(BaseModel):
@@ -16,10 +18,13 @@ class Bill(BaseModel):
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     images = Column(ARRAY(String), default=list)
-    generating_items = Column(Boolean, default=False, nullable=False)
-    generation_successful = Column(Boolean, default=False, nullable=True)
 
     items = relationship("Item", back_populates="bill", uselist=True)
+    item_generations = relationship(
+        "ItemGeneration",
+        back_populates="bill",
+        uselist=True,
+    )
     participants = relationship("Participant", back_populates="bill", uselist=True)
 
     @property
@@ -27,3 +32,21 @@ class Bill(BaseModel):
         if not self.images:
             return None
         return self.images[-1]
+
+    @property
+    def item_generation(self) -> ItemGeneration | None:
+        if not self.item_generations:
+            return None
+        return self.item_generations[-1]
+
+    @property
+    def generating_items(self) -> bool:
+        if self.item_generation is None:
+            return False
+        return self.item_generation.running
+
+    @property
+    def generation_successful(self) -> bool | None:
+        if self.item_generation is None:
+            return None
+        return self.item_generation.generation_successful
