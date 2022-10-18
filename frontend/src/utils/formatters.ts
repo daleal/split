ce6c48aaf769a2309c9d7c00aab6fc1da7ccf20e
Daniel Leal <cosaleal@gmail.com>
2022-10-18 19:54:04 -0300

    Change the formatting of decimals to fit with the Chilean standard

--- frontend/src/utils/formatters.ts
+++ frontend/src/utils/formatters.ts
@@ -1,12 +1,16 @@
+import { numbersFormatter } from '@/utils/intl';
+
 export const consumptionAmountFormatter = (newValue: string, oldValue: string) => {
-  const validAmountExpression = /^[0-9]+\.?[0-9]{0,2}$/;
-  const cleaned = newValue.trim().replace(',', '.');
+  const validAmountExpression = /^[0-9]+,?[0-9]{0,2}$/;
+  const cleaned = newValue.trim().replace('.', ',');
   if (!cleaned) {
     return '0';
   }
   if (validAmountExpression.test(cleaned)) {
-    const trailingCharacter = cleaned.at(-1) === '.' ? '.' : '';
-    return Number.parseFloat(cleaned).toString() + trailingCharacter;
+    const trailingCharacter = cleaned.at(-1) === ',' ? ',' : '';
+    return numbersFormatter.format(
+      Number.parseFloat(cleaned.replace(',', '.')),
+    ) + trailingCharacter;
   }
   return oldValue;
 };
