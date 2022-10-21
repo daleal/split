5fb939f925605a8924bdd7c31af2adbe272b4c76
Daniel Leal <cosaleal@gmail.com>
2022-10-21 20:15:07 -0300

    Transform spaces after dots in between numbers to just a dot

--- backend/split/services/receipt_parser/cleaner.py
+++ backend/split/services/receipt_parser/cleaner.py
@@ -48,7 +48,10 @@ REGEX_TO_REPLACE_WITH_SPACE = re.compile(
 )
 
 CUSTOM_REPLACEMENTS = [
-    lambda text: re.sub(r"((^|\s+)\d+)(\.|,)\d{1,2}($|\s+)", r"\1\4", text),  # decimals
+    # dot + space in between only digits to only dot
+    lambda text: re.sub(r"((^|\s+)\d+)(\.|,) (\d+)($|\s+)", r"\1\3\4\5", text),
+    # two decimals to only integer
+    lambda text: re.sub(r"((^|\s+)\d+)(\.|,)\d{1,2}($|\s+)", r"\1\4", text),
 ]
 
 
