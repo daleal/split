156f6f6e4d7f3577ea0c8f01f9935dc3d3da794f
Daniel Leal <cosaleal@gmail.com>
2022-12-07 03:29:57 -0300

    Allow numbers on strings

--- backend/tests/services/receipt_parser/test_parser.py
+++ backend/tests/services/receipt_parser/test_parser.py
@@ -1,5 +1,3 @@
-import pytest
-
 from split.services.receipt_parser.parser import search
 
 
@@ -14,299 +12,245 @@ def assert_values(
 
 class TestPriceSearch:
     def test_price_description(self):
-        text = "1000 hot dog"
-        parsed = search(text)
+        parsed = search("1000 hot dog")
         assert_values(parsed, {"description": "hot dog", "full_price": 1000})
 
     def test_description_price(self):
-        text = "hot dog 1000"
-        parsed = search(text)
+        parsed = search("hot dog 1000")
         assert_values(parsed, {"description": "hot dog", "full_price": 1000})
 
     def test_low_price_high_price_description(self):
-        text = "1000 2000 hot dog"
-        parsed = search(text)
+        parsed = search("1000 2000 hot dog")
         assert_values(parsed, {"description": "hot dog", "full_price": 2000})
 
     def test_description_low_price_high_price(self):
-        text = "hot dog 1000 2000"
-        parsed = search(text)
+        parsed = search("hot dog 1000 2000")
         assert_values(parsed, {"description": "hot dog", "full_price": 2000})
 
     def test_high_price_low_price_description(self):
-        text = "2000 1000 hot dog"
-        parsed = search(text)
+        parsed = search("2000 1000 hot dog")
         assert_values(parsed, {"description": "hot dog", "full_price": 2000})
 
     def test_description_high_price_low_price(self):
-        text = "hot dog 2000 1000"
-        parsed = search(text)
+        parsed = search("hot dog 2000 1000")
         assert_values(parsed, {"description": "hot dog", "full_price": 2000})
 
 
 class TestAmountPriceOnlyTextDescriptionSearch:
     def test_amount_description_price(self):
-        text = "3 hot dog 1000"
-        parsed = search(text)
+        parsed = search("3 hot dog 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 1000}
         )
 
     def test_price_description_amount(self):
-        text = "1000 hot dog 3"
-        parsed = search(text)
+        parsed = search("1000 hot dog 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 1000}
         )
 
     def test_description_amount_price(self):
-        text = "hot dog 3 1000"
-        parsed = search(text)
+        parsed = search("hot dog 3 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 1000}
         )
 
     def test_description_price_amount(self):
-        text = "hot dog 1000 3"
-        parsed = search(text)
+        parsed = search("hot dog 1000 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 1000}
         )
 
     def test_amount_description_low_price_high_price(self):
-        text = "3 hot dog 1000 2000"
-        parsed = search(text)
+        parsed = search("3 hot dog 1000 2000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_low_price_high_price_description_amount(self):
-        text = "1000 2000 hot dog 3"
-        parsed = search(text)
+        parsed = search("1000 2000 hot dog 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_description_amount_low_price_high_price(self):
-        text = "hot dog 3 1000 2000"
-        parsed = search(text)
+        parsed = search("hot dog 3 1000 2000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_description_low_price_high_price_amount(self):
-        text = "hot dog 1000 2000 3"
-        parsed = search(text)
+        parsed = search("hot dog 1000 2000 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_amount_description_high_price_low_price(self):
-        text = "3 hot dog 2000 1000"
-        parsed = search(text)
+        parsed = search("3 hot dog 2000 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_high_price_low_price_description_amount(self):
-        text = "2000 1000 hot dog 3"
-        parsed = search(text)
+        parsed = search("2000 1000 hot dog 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_description_amount_high_price_low_price(self):
-        text = "hot dog 3 2000 1000"
-        parsed = search(text)
+        parsed = search("hot dog 3 2000 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
     def test_description_high_price_low_price_amount(self):
-        text = "hot dog 2000 1000 3"
-        parsed = search(text)
+        parsed = search("hot dog 2000 1000 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog", "full_price": 2000}
         )
 
 
 class TestAmountPriceSearch:  # pylint: disable=too-many-public-methods
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_amount_description_number_price(self):
-        text = "3 hot dog 1 1000"
-        parsed = search(text)
+        parsed = search("3 hot dog 1 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 1000}
         )
 
     def test_amount_number_description_price(self):
-        text = "3 1 hot dog 1000"
-        parsed = search(text)
+        parsed = search("3 1 hot dog 1000")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 1000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_price_description_number_amount(self):
-        text = "1000 hot dog 1 3"
-        parsed = search(text)
+        parsed = search("1000 hot dog 1 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 1000}
         )
 
     def test_price_number_description_amount(self):
-        text = "1000 1 hot dog 3"
-        parsed = search(text)
+        parsed = search("1000 1 hot dog 3")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 1000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_description_number_amount_price(self):
-        text = "hot dog 1 3 1000"
-        parsed = search(text)
+        parsed = search("hot dog 1 3 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 1000}
         )
 
     def test_number_description_amount_price(self):
-        text = "1 hot dog 3 1000"
-        parsed = search(text)
+        parsed = search("1 hot dog 3 1000")
         assert_values(
-            parsed, {"amount": 3, "description": "1 hot dog", "full_price": 1000}
+            parsed, {"amount": 1, "description": "hot dog 3", "full_price": 1000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_description_number_price_amount(self):
-        text = "hot dog 1 1000 3"
-        parsed = search(text)
+        parsed = search("hot dog 1 1000 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 1000}
         )
 
     def test_number_description_price_amount(self):
-        text = "1 hot dog 1000 3"
-        parsed = search(text)
+        parsed = search("1 hot dog 1000 3")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 1000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_amount_description_number_low_price_high_price(self):
-        text = "3 hot dog 1 1000 2000"
-        parsed = search(text)
+        parsed = search("3 hot dog 1 1000 2000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_amount_number_description_low_price_high_price(self):
-        text = "3 1 hot dog 1000 2000"
-        parsed = search(text)
+        parsed = search("3 1 hot dog 1000 2000")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_low_price_high_price_description_number_amount(self):
-        text = "1000 2000 hot dog 1 3"
-        parsed = search(text)
+        parsed = search("1000 2000 hot dog 1 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_low_price_high_price_number_description_amount(self):
-        text = "1000 2000 1 hot dog 3"
-        parsed = search(text)
+        parsed = search("1000 2000 1 hot dog 3")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_description_number_amount_low_price_high_price(self):
-        text = "hot dog 1 3 1000 2000"
-        parsed = search(text)
+        parsed = search("hot dog 1 3 1000 2000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_number_description_amount_low_price_high_price(self):
-        text = "1 hot dog 3 1000 2000"
-        parsed = search(text)
+        parsed = search("1 hot dog 3 1000 2000")
         assert_values(
-            parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
+            parsed, {"amount": 1, "description": "hot dog 3", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_description_number_low_price_high_price_amount(self):
-        text = "hot dog 1 1000 2000 3"
-        parsed = search(text)
+        parsed = search("hot dog 1 1000 2000 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_number_description_low_price_high_price_amount(self):
-        text = "1 hot dog 1000 2000 3"
-        parsed = search(text)
+        parsed = search("1 hot dog 1000 2000 3")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_amount_description_number_high_price_low_price(self):
-        text = "3 hot dog 1 2000 1000"
-        parsed = search(text)
+        parsed = search("3 hot dog 1 2000 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_amount_number_description_high_price_low_price(self):
-        text = "3 1 hot dog 2000 1000"
-        parsed = search(text)
+        parsed = search("3 1 hot dog 2000 1000")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_high_price_low_price_description_number_amount(self):
-        text = "2000 1000 hot dog 1 3"
-        parsed = search(text)
+        parsed = search("2000 1000 hot dog 1 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_high_price_low_price_number_description_amount(self):
-        text = "2000 1000 1 hot dog 3"
-        parsed = search(text)
+        parsed = search("2000 1000 1 hot dog 3")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_description_number_amount_high_price_low_price(self):
-        text = "hot dog 1 3 2000 1000"
-        parsed = search(text)
+        parsed = search("hot dog 1 3 2000 1000")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_number_description_amount_high_price_low_price(self):
-        text = "1 hot dog 3 2000 1000"
-        parsed = search(text)
+        parsed = search("1 hot dog 3 2000 1000")
         assert_values(
-            parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
+            parsed, {"amount": 1, "description": "hot dog 3", "full_price": 2000}
         )
 
-    @pytest.mark.xfail(reason="Case not implemented")
     def test_description_number_high_price_low_price_amount(self):
-        text = "hot dog 1 2000 1000 3"
-        parsed = search(text)
+        parsed = search("hot dog 1 2000 1000 3")
         assert_values(
             parsed, {"amount": 3, "description": "hot dog 1", "full_price": 2000}
         )
 
     def test_number_description_high_price_low_price_amount(self):
-        text = "1 hot dog 2000 1000 3"
-        parsed = search(text)
+        parsed = search("1 hot dog 2000 1000 3")
         assert_values(
             parsed, {"amount": 3, "description": "1 hot dog", "full_price": 2000}
         )
